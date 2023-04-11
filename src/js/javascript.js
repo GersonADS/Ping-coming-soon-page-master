e99fe8f401dca6200654fb312aa8a59a85f1a4c5
GersonADS <shiranuime@gmail.com>
2023-04-11 16:49:32 -0300

    finalizating js

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -3,6 +3,8 @@
 const $Email = document.querySelector('#_email');
 const $Button = document.querySelector('.btn');
 
+
+
 let RegExp_ = new RegExp('^[A-Z a-z 0-9]{5,10}[@]+[a-z]{4,}.[a-z]{2,3}$');
 
 $Button.addEventListener('click',()=>{
@@ -10,12 +12,9 @@ $Button.addEventListener('click',()=>{
     let Emails = RegExp_.test($Email.value);
     
     console.log(Emails)
-
-    if(typeof Emails === false ){
-        console.log('ta pegando')
-        // $Email.style.border= '2px solid black';
-
-    }
+    let $Alert = document.getElementById("alert");
+    $Alert.innerText  = "insira um e-mail valido";
+    $Alert.style.color = 'hsl(354, 100%, 66%)';
 
 });
 
